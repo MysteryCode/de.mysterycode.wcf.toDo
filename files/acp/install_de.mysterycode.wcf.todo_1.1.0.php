5f450e5d89b7ed115c52821a21dc3d42791964b8
FloGa96 <florian@mysterycode.de>
2014-10-03 13:39:54 +0200

    workaround for activating dashboardboxes without errors after faulty install

--- files/acp/install_de.mysterycode.wcf.todo_1.1.0.php
+++ files/acp/install_de.mysterycode.wcf.todo_1.1.0.php
@@ -4,18 +4,35 @@ use wcf\system\WCF;
 
 $package = $this->installation->getPackage();
 
-// set default values for dashboard boxes
-DashboardHandler::setDefaultValues('de.mysterycode.wcf.ToDoListPage', array(
-	'de.mysterycode.wcf.toDo.outstanding' => 1,
-	'de.mysterycode.wcf.toDo.statistics' => 2
-));
-DashboardHandler::setDefaultValues('de.mysterycode.wcf.ToDoCategoryPage', array(
-	'de.mysterycode.wcf.toDo.outstanding' => 1,
-	'de.mysterycode.wcf.toDo.statistics' => 2
+// workaround because wcf doesn't prove
+$sql = "SELECT *
+	FROM		wcf".WCF_N."_dashboard_option
+	WHERE		objectTypeID = (SELECT objectTypeID FROM wcf".WCF_N."_object_type WHERE objectType = ? AND definitionID = (SELECT definitionID FROM wcf".WCF_N."_object_type_definition WHERE definitionName = ?))
+			AND boxID = (SELECT boxID FROM wcf".WCF_N."_dashboard_box WHERE boxName = ? or boxName = ?)";
+$statement = WCF::getDB()->prepareStatement($sql);
 
-));
+$statement->execute(array('de.mysterycode.wcf.ToDoListPage', 'com.woltlab.wcf.user.dashboardContainer', 'de.mysterycode.wcf.toDo.outstanding', 'de.mysterycode.wcf.toDo.statistics'));
+$row = $statement->fetchArray();
+if ($row === false) {
+	// set default values for dashboard boxes
+	DashboardHandler::setDefaultValues('de.mysterycode.wcf.ToDoListPage', array(
+		'de.mysterycode.wcf.toDo.outstanding' => 1,
+		'de.mysterycode.wcf.toDo.statistics' => 2
+	));
+}
+
+$statement->execute(array('de.mysterycode.wcf.ToDoCategoryPage', 'com.woltlab.wcf.user.dashboardContainer', 'de.mysterycode.wcf.toDo.outstanding', 'de.mysterycode.wcf.toDo.statistics'));
+$row = $statement->fetchArray();
+if ($row === false) {
+	// set default values for dashboard boxes
+	DashboardHandler::setDefaultValues('de.mysterycode.wcf.ToDoCategoryPage', array(
+		'de.mysterycode.wcf.toDo.outstanding' => 1,
+		'de.mysterycode.wcf.toDo.statistics' => 2
+	));
+}
 
 // unset notification settings for creating todos - also needed when reinstalling
+// workaround
 $sql = "DELETE FROM	wcf".WCF_N."_user_notification_event_to_user
 	WHERE		eventID = (SELECT eventID FROM wcf".WCF_N."_user_notification_event WHERE eventName = ? AND className = ?)";
 $statement = WCF::getDB()->prepareStatement($sql);
