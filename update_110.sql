e5cc3d8b43976b2b79dc45480a4651c48ded06b9
FloGa96 <florian@mysterycode.de>
2014-10-03 17:12:51 +0200

    fixed issue with userid = 0

--- update_110.sql
+++ update_110.sql
@@ -1,2 +1,3 @@
 ALTER TABLE wcf1_todo		CHANGE	submitter	submitter	int(10);
+UPDATE TABLE wcf1_todo		SET submitter = NULL	WHERE submitter = 0;
 ALTER TABLE wcf1_todo		ADD	FOREIGN KEY (submitter) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
