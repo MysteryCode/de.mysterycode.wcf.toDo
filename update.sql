faa0096656fcaa9b69f1b510c486eddb3bcbf442
FloGa96 <florian@mysterycode.de>
2014-11-08 16:24:04 +0100

    fixed update query

--- update.sql
+++ update.sql
@@ -1,2 +1,3 @@
-ALTER TABLE wcf1_todo		CHANGE	deletedByID	deletedByID	int(10);
+ALTER TABLE	wcf1_todo		CHANGE	deletedByID	deletedByID	int(10);
+UPDATE	 	wcf1_todo		SET	deletedByID = NULL	WHERE	deletedByID = 0;
 ALTER TABLE wcf1_todo ADD FOREIGN KEY (deletedByID) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
