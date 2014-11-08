7ec250134b27c30e7ebe11b6d5972c0887252b5b
FloGa96 <florian@mysterycode.de>
2014-11-08 15:42:13 +0100

    deleted obsolete update stuff

--- update_110.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE wcf1_todo		CHANGE	submitter	submitter	int(10);
-UPDATE wcf1_todo			SET submitter = NULL	WHERE submitter = 0;
-ALTER TABLE wcf1_todo		ADD	FOREIGN KEY (submitter) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
