982d43ed167ec8d0640d68476e545242b14f33d6
FloGa96 <florian@mysterycode.de>
2014-11-10 11:16:08 +0100

    edit

--- files/lib/data/todo/category/TodoCategory.class.php
+++ files/lib/data/todo/category/TodoCategory.class.php
@@ -14,10 +14,11 @@ use wcf\system\WCF;
 /**
  * Represents a todo-category.
  *
- * @author	Sebastian Oettl
- * @package	com.wcfsolutions.wsif
- * @subpackage	data.category
- * @category	Infinite Filebase
+ * @author	Florian Gail
+ * @copyright	2014 Florian Gail <http://www.mysterycode.de/>
+ * @license	Kostenlose Plugins <http://downloads.mysterycode.de/index.php/License/6-Kostenlose-Plugins/>
+ * @package	de.mysterycode.wcf.toDo
+ * @category 	WCF
  */
 class TodoCategory extends AbstractDecoratedCategory implements IBreadcrumbProvider, IRouteController, ILinkableObject {
 	/**
