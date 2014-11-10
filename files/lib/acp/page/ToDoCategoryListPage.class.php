46ba4ed493977d3b3732e9185643a517b0a3d81c
FloGa96 <florian@mysterycode.de>
2014-11-10 12:54:10 +0100

    updated todo category page

--- files/lib/acp/page/ToDoCategoryListPage.class.php
+++ files/lib/acp/page/ToDoCategoryListPage.class.php
@@ -1,11 +1,9 @@
 <?php
 namespace wcf\acp\page;
-use wcf\page\AbstractPage;
-use wcf\system\WCF;
-use wcf\util\StringUtil;
+use wcf\acp\page\AbstractCategoryListPage;
 
 /**
- * Shows the toDoCategoryList page.
+ * Shows the todo category list page.
  *
  * @author	Florian Gail
  * @copyright	2014 Florian Gail <http://www.mysterycode.de/>
@@ -13,37 +11,19 @@ use wcf\util\StringUtil;
  * @package	de.mysterycode.wcf.toDo
  * @category 	WCF
  */
-
-class ToDoCategoryListPage extends AbstractPage {
+class ToDoCategoryListPage extends AbstractCategoryListPage {
 	/**
-	 * @see	wcf\page\AbstractPage::$activeMenuItem
+	 * @see	\wcf\page\AbstractPage::$activeMenuItem
 	 */
 	public $activeMenuItem = 'wcf.acp.menu.link.toDoCategory.list';
 	
-	public $categoryList = array();
-
 	/**
-	 * @see wcf\page\IPage::assignVariables()
+	 * @see	\wcf\acp\page\AbstractCategoryListPage::$objectTypeName
 	 */
-	public function assignVariables() {
-		parent::assignVariables();
-		
-		$sql = "SELECT *
-			FROM wcf" . WCF_N . "_todo_category
-			ORDER BY title ASC";
-		$statement = WCF::getDB()->prepareStatement($sql);
-		$statement->execute();
-		
-		while ($row = $statement->fetchArray()) {
-			$this->categoryList[] = array(
-				'id' => $row['id'],
-				'title' => $row['title'],
-				'color' => $row['color']
-			);
-		}
-		
-		WCF::getTPL()->assign(array(
-			'categoryList' => $this->categoryList
-		));
-	}
+	public $objectTypeName = 'de.mysterycode.wcf.toDo';
+	
+	/**
+	 * @see	\wcf\acp\page\AbstractCategoryListPage::$pageTitle
+	 */
+	public $pageTitle = 'wcf.acp.todo.category.list';
 }
