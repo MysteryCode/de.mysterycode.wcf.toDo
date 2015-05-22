66f3a6bae2fa9d7cfd1130d0e3cad24b2cb0caac
mutec <florian@mysterycode.de>
2015-05-22 17:27:56 +0200

    implemented sorting by showOrder of new status DBOs

--- files/lib/data/todo/ToDoList.class.php
+++ files/lib/data/todo/ToDoList.class.php
@@ -15,31 +15,36 @@ use wcf\system\WCF;
  */
 class ToDoList extends DatabaseObjectList {
 	/**
-	 *
 	 * @see \wcf\data\DatabaseObjectList::$className
 	 */
 	public $className = 'wcf\data\todo\ToDo';
 	
 	/**
-	 *
 	 * @see \wcf\data\DatabaseObjectList::__construct()
 	 */
 	public function __construct() {
 		parent::__construct ();
 		
-		if (!empty($this->sqlSelects)) $this->sqlSelects .= ',';
-		$this->sqlSelects .= "todo_category.title as categorytitle, todo_category.color as categorycolor";
+		if (!empty($this->sqlSelects))
+			$this->sqlSelects .= ',';
+		$this->sqlSelects .= " todo_category.title as categorytitle, todo_category.color as categorycolor";
 		$this->sqlJoins = " LEFT JOIN wcf" . WCF_N . "_todo_category todo_category ON (todo_table.category = todo_category.id)";
+		
 		// visible status
 		if (!WCF::getSession()->getPermission('mod.toDo.canViewDeleted')) {
 			$this->getConditionBuilder()->add('todo_table.isDeleted = 0');
 			$this->loadDeleteNote = false;
 		}
-		if (!WCF::getSession()->getPermission('mod.toDo.canEnable')) {
+		
+		if ($this->sqlOrderBy == 'status ASC' || $this->sqlOrderBy == 'status DESC') {
+			$this->sqlJoins = " LEFT JOIN wcf" . WCF_N . "_todo_status status ON (todo_table.statusID = status.statusID)";
+			$this->sqlOrderBy = ($this->sqlOrderBy == 'status ASC' ? 'status.showOrder ASC' : 'status.showOrder DESC');
+		}
+		
+		if (!WCF::getSession()->getPermission('mod.toDo.canEnable'))
 			$this->getConditionBuilder()->add('todo_table.isDisabled = 0');
-		}
-		if (!WCF::getSession()->getPermission('user.toDo.toDo.canViewPrivate')) {
+		
+		if (!WCF::getSession()->getPermission('user.toDo.toDo.canViewPrivate'))
 			$this->getConditionBuilder()->add("(private = ? or submitter = ? or (SELECT assigns.toDoID FROM wcf" . WCF_N . "_todo_to_user assigns WHERE assigns.toDoID = todo_table.id AND assigns.userID = ?) = todo_table.id)", array (0, WCF::getUser()->userID, WCF::getUser()->userID));
-		}
 	}
 }
