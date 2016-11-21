f652bbf468204463eca8225fa72cb76f09d982c1
mutec <webmaster@mysterycode.de>
2016-11-21 22:18:29 +0100

    remove update files

--- update_12beta2.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE wcf1_todo CHANGE title title varchar(255) NOT NULL DEFAULT '';
-ALTER TABLE wcf1_todo CHANGE description description text;
-ALTER TABLE wcf1_todo CHANGE note note text;
-ALTER TABLE wcf1_todo ADD ipAddress varchar(39) NOT NULL DEFAULT '';
-
-ALTER TABLE wcf1_todo DROP html_description;
-ALTER TABLE wcf1_todo DROP html_notes;
--- update_post.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE wcf1_todo DROP FOREIGN KEY category;
-ALTER TABLE wcf1_todo DROP category;
-ALTER TABLE wcf1_todo DROP status;
-ALTER TABLE wcf1_todo DROP html_description;
-ALTER TABLE wcf1_todo DROP html_notes;
-
-DROP TABLE IF EXISTS wcf1_todo_category;
