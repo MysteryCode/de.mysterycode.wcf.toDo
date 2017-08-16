a492a62e79d284096bb1807ac5e5182ab4f72f81
mutec <webmaster@mysterycode.de>
2017-08-16 15:59:31 +0200

    typo and minified JS
    see also https://support.mysterycode.de/thread/657/

--- files/js/WCF.Todo.js
+++ files/js/WCF.Todo.js
@@ -2,7 +2,7 @@
  * Todo-related classes.
  * 
  * @author	Florian Gail
- * @copyright	2014 Florian Gail <http://www.mysterycode.de/>
+ * @copyright	2014-2017 Florian Gail <http://www.mysterycode.de/>
  * @license	Kostenlose Plugins <http://downloads.mysterycode.de/index.php/License/6-Kostenlose-Plugins/>
  */
 
