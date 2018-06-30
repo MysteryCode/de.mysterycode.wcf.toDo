1b37a3094251f9777e81ea5952ff03709e9aae73
mutec <webmaster@mysterycode.de>
2018-07-01 00:16:12 +0200

    update like provider

--- files/lib/data/todo/LikeableToDoProvider.class.php
+++ files/lib/data/todo/LikeableToDoProvider.class.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace wcf\data\todo;
+use wcf\data\like\IRestrictedLikeObjectTypeProvider;
 use wcf\data\like\object\ILikeObject;
-use wcf\data\like\ILikeObjectTypeProvider;
 use wcf\data\object\type\AbstractObjectTypeProvider;
+use wcf\system\like\IViewableLikeProvider;
+use wcf\system\WCF;
 
 /**
  * Object type provider for likable todos.
@@ -13,7 +15,7 @@ use wcf\data\object\type\AbstractObjectTypeProvider;
  * @license	Kostenlose Plugins <https://www.mysterycode.de/licenses/kostenlose-plugins/>
  * @package	de.mysterycode.wcf.toDo
  */
-class LikeableToDoProvider extends AbstractObjectTypeProvider implements ILikeObjectTypeProvider {
+class LikeableToDoProvider extends AbstractObjectTypeProvider implements IRestrictedLikeObjectTypeProvider, IViewableLikeProvider {
 	/**
 	 * @inheritDoc
 	 */
@@ -31,8 +33,62 @@ class LikeableToDoProvider extends AbstractObjectTypeProvider implements ILikeOb
 	
 	/**
 	 * @inheritDoc
+	 * @param LikeableToDo $object
 	 */
 	public function checkPermissions(ILikeObject $object) {
 		return $object->canEnter();
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function canLike(ILikeObject $object) {
+		return $this->checkPermissions($object) && ($object->getUserID() !== WCF::getUser()->userID || !LIKE_ALLOW_FOR_OWN_CONTENT);
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function canViewLikes(ILikeObject $object) {
+		return $this->checkPermissions($object);
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function prepare(array $likes) {
+		$entryIDs = [];
+		foreach ($likes as $like) {
+			$entryIDs[] = $like->objectID;
+		}
+		
+		// fetch todos
+		$todoList = new ViewableToDoList();
+		$todoList->setObjectIDs($entryIDs);
+		$todoList->readObjects();
+		$todos = $todoList->getObjects();
+		
+		// set message
+		foreach ($likes as $like) {
+			if (isset($todos[$like->objectID])) {
+				$todo = $todos[$like->objectID];
+				
+				// check permissions
+				if (!$todo->isVisible()) {
+					continue;
+				}
+				$like->setIsAccessible();
+				
+				// short output
+				$text = WCF::getLanguage()->getDynamicVariable('wcf.like.title.de.mysterycode.mcgb.likeableEntry', [
+					'todo' => $todo,
+					'like' => $like
+				]);
+				$like->setTitle($text);
+				
+				// output
+				$like->setDescription($todo->getSimplifiedFormattedMessage());
+			}
+		}
 	}
 }
